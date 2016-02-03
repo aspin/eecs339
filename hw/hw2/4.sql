4f5b530612540861423cae244afba8f5c22970fb
aspin <tsunami70875@gmail.com>
2016-02-03 14:49:46 -0600

    fix for no sections

--- hw/hw2/4.sql
+++ hw/hw2/4.sql
@@ -1,4 +1,4 @@
 SELECT C.cno, C.cname, E.sectno FROM course C
-    JOIN enroll E ON E.cno = C.cno AND E.dname = C.dname -- LEFT for null sect
+    LEFT JOIN enroll E ON E.cno = C.cno AND E.dname = C.dname -- LEFT for null sect, as per piazza
     GROUP BY C.cno, C.cname, E.sectno
     HAVING COUNT(E.cno) < 6;
