c6cc66acd67710e1f90488087d70db58bb5af8b7
aspin <tsunami70875@gmail.com>
2016-02-04 11:35:00 -0600

    minor fixes to hw

--- hw/hw2/4.sql
+++ hw/hw2/4.sql
@@ -1,4 +1,6 @@
+-- As per Piazza, LEFT JOIN to also include the class with no students in enrolled
+-- Remove the LEFT for the answer for only classes with students
 SELECT C.cno, C.cname, E.sectno FROM course C
-    LEFT JOIN enroll E ON E.cno = C.cno AND E.dname = C.dname -- LEFT for null sect, as per piazza
+    LEFT JOIN enroll E ON E.cno = C.cno AND E.dname = C.dname
     GROUP BY C.cno, C.cname, E.sectno
     HAVING COUNT(E.cno) < 6;
--- hw/hw2/8.sql
+++ hw/hw2/8.sql
@@ -1,2 +1,2 @@
 SELECT MAX(S.age) - MIN(S.age) as range  FROM student S, major M
-    WHERE S.sid = M.sid AND M.dname = 'Computer Science'
+    WHERE S.sid = M.sid AND M.dname = 'Computer Science';
